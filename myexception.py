f30d007d3f31c01c77b54b22d650df9695de4a6c
Charles Chi <12949554+CharlesMaybach@users.noreply.github.com>
2019-11-30 03:54:22 -0500

    Added index feature

---
+++ myexception.py
@@ -0,0 +1,17 @@
+class PrintException:
+
+    @staticmethod
+    def dimensionError():
+        print('Dimension not match.')
+    
+    @staticmethod
+    def keyError():
+        print('Key not in dist!')
+
+    @staticmethod
+    def typeError():
+        print('')
+
+    @staticmethod
+    def indexError():
+        print('Index does not exist.')
