9a21187f110efd04f18007847e3d28ace664d827
Charles Chi <12949554+CharlesMaybach@users.noreply.github.com>
2019-12-02 02:36:17 -0500

    Add files via upload

--- myparser.py
+++ myparser.py
@@ -1,34 +1,11 @@
-from utils.utils import Param
-from utils.lex_header import Symbol
 from utils.myexception import PrintException
 from src.table import Table
 import re
 
 class Parser:
 
-    def __init__(self, trie, terminate_map = None):
-        self.trie = trie
-    
-    def _tokenize(self, word):
-        if self.trie.search(word):
-            terminate = Param.terminate_pair[self.trie.search(word)]
-        else:
-            if word in Param.punctuation:
-                terminate  = 'Delimiter'
-            else:
-                try:
-                    int(word)
-                    terminate = 'Value'
-                except:
-                    terminate = 'Identifier'
-        return Symbol(word, terminate)
-
-    def _split(self, string):
-        parts = string.split()
-
-        # TODO: Handle multiple keywords
-
-        return list(map(self._tokenize, parts))
+    def __init__(self):
+        pass
 
     def parse(self, string):
         tokens = re.split("\\s", string.strip())
